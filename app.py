3b8d3c42a6590d1b563da3cd67d79ecfc78aa659
yadavshalini1715-crypto <yadavshalini1715@gmail.com>
2026-07-08 11:11:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print("machine learning based buyer segmentation and investment profilin")
+import pandas as pd 
+from sklearn.cluster import KMeans
+#load dataset 
+data=pd.read_csv("buyer_data.csv")
+# Display first 5 rows
+print(data.head())
+# Select features 
+x= data[['age','income']]
+# Create K-Means model 
+model = KMeans (n_clusters=3, randaom_state=42)
+model.fit(x)
+#Add cluster labels
+data ['cluster']= model.labels_
+#show result
+print(data)
